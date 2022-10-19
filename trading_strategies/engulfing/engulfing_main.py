592a9ba85a2e927099e487534b2231461240f570
SaraSTEM | Trading Algorítmico <64358108+SaraStemTrading@users.noreply.github.com>
2022-10-19 14:13:19 +0200

    Update engulfing_main.py

--- trading_strategies/engulfing/engulfing_main.py
+++ trading_strategies/engulfing/engulfing_main.py
@@ -2,21 +2,33 @@ from engulfing_backtesting import strategy_engulfing
 
 
 def results():
+    strategy_engulfing.info()
     data = strategy_engulfing.strategy_body()
     stats = strategy_engulfing.run_bt(data, strategy_engulfing)
     results = {}
     return_ = round(stats[6], 2)
+    return_buy_hold = round(stats[7], 2)
+    return_ann = round(stats[8], 2)
     n_op = stats[17]
     win_ratio = round(stats[18], 2)
     drawdown = round(stats[13], 2)
     volatility = round(stats[9], 2)
     sharpe_ratio = round(stats[10], 2)
-    results = {'Rentabilidad (%)': return_, 'Nº trades': n_op, 'Ratio de aciertos (%)': win_ratio, 'Máx. Drawdown (%)': drawdown,
-               'Volatilidad (%)': volatility, 'Ratio Sharpe': sharpe_ratio}
+    sortino_ratio = round(stats[11], 2)
+    calmar_ratio = round(stats[12], 2)
+    profit_factor = round(stats[24], 2)
+    sqn = round(stats[26], 2)
+    dd_duration = stats[15]
+    df_dd = stats._equity_curve
+    dd_peak = df_dd.iloc[len(df_dd)-1]['DrawdownDuration']
+    results = {'Rentabilidad (%)': return_, 'Rentabilidad anualizada (%)': return_ann, 'Número de operaciones': n_op, 'Ratio de aciertos (%)': win_ratio, 'Máximo Drawdown (%)': drawdown,
+               'Volatilidad (%)': volatility, 'Ratio Sharpe': sharpe_ratio, 'Ratio Sortino': sortino_ratio,
+               'Ratio Calmar': calmar_ratio, 'Factor de beneficio': profit_factor, 'Calidad': sqn, 'Benchmark': return_buy_hold,
+               'Caída desde el pico': dd_peak, 'Duración DD': dd_duration}
     [print(key, ':', value) for key, value in results.items()]
 
     return results
 
+
 if __name__ == "__main__":
-    strategy_engulfing.info()
     results()
